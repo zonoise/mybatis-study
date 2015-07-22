0fef07d433809c9067ad1f44f01a4f1122d35a9e
zonoise <zonoise@gmail.com>
2015-07-22 20:13:39 +0900

    specify main class

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -1,10 +1,12 @@
 package com.example.app;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 
 /**
  * Created by zonoise on 2015/07/17.
@@ -16,6 +18,15 @@ public class App {
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
         SqlSessionFactory factory = builder.build(inputStream);
+        SqlSession sqlSession = factory.openSession();
+
+        PostMapper postMapper = sqlSession.getMapper(PostMapper.class);
+        Post p = new Post();
+        p.setId((new Date()).getTime());
+        p.setTitle("title");
+        postMapper.insert(p);
+        sqlSession.commit();
+        sqlSession.close();
         System.out.println("hello");
     }
 }
